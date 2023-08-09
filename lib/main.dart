8e2db63e5bd5f94da2d4a1f1f7ad4c873f99cfb4
mohamme alorf <mohammed1alorf@gmail.com>
2023-08-09 22:21:16 +0100

    Responsive UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:multiple_calculators/container_button.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 void main() {
   runApp(const Myapp());
 }
@@ -10,75 +12,78 @@ class Myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: Scaffold(
-      //Background Container color
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-              Color.fromARGB(255, 255, 174, 204),
-              Color.fromARGB(255, 255, 233, 241)
-            ])),
-        height: double.infinity,
-        width: double.infinity,
-        //Coulmn inside Background Container
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Hello Again!',
-              style:
-                  GoogleFonts.inter(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(
-              'You\'ve been missed',
-              style: GoogleFonts.inter(
-                  fontSize: 16, fontWeight: FontWeight.normal),//
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(40),
-              //White Continer
-              child: Container(
-                color: const Color.fromARGB(255, 0, 0, 0),
-                height: 440,
-                width: 333,
-                //Column inside the white Container
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [    
-                    Text(
-                      'What is Your need?',
-                      style: GoogleFonts.inter(
-                          fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-
-                    //Container Buttons
-                    const ContainerButton1(),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const ContainerButton2(),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const ContainerButton3()
-                  ],
+    return ScreenUtilInit(
+      builder: (context, child) => MaterialApp(
+        home: Scaffold(
+          //Background Container color
+          body: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                  Color.fromARGB(255, 255, 174, 204),
+                  Color.fromARGB(255, 255, 233, 241)
+                ])),
+            height: double.infinity,
+            width: double.infinity,
+            //Coulmn inside Background Container
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Hello Again!',
+                  style: GoogleFonts.inter(
+                      fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-              ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  'You\'ve been missed',
+                  style: GoogleFonts.inter(
+                      fontSize: 16, fontWeight: FontWeight.normal), //
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(40),
+                  //White Continer
+                  child: Container(
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                    height: 440,
+                    width: 333,
+                    //Column inside the white Container
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'What is Your need?',
+                          style: GoogleFonts.inter(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+
+                        //Container Buttons
+                        const ContainerButton1(),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        const ContainerButton2(),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        const ContainerButton3()
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
-    ),
-  );
+      designSize: const Size(852, 393),
+    );
   }
 }
